437eb15b74ca864dc10f865a500265dadf71bb9d
Scott Eberhardt <eberhardtscott33@gmail.com>
2026-08-06 02:53:36 -0700

    fix: add resolveIPFS and realtime named exports

--- apps/web/src/lib/ipfs.ts
+++ apps/web/src/lib/ipfs.ts
@@ -1,4 +1,9 @@
 
+export function resolveIPFS(cid: string): string {
+  if (!cid) return "";
+  if (cid.startsWith("http")) return cid;
+  return `https://ipfs.io/ipfs/${cid}`;
+}
 export async function uploadToIPFS(data: any): Promise<string> { return ""; }
 export async function getFromIPFS(cid: string): Promise<any> { return null; }
-export default { uploadToIPFS, getFromIPFS };
+export default { resolveIPFS, uploadToIPFS, getFromIPFS };
--- apps/web/src/lib/realtime.ts
+++ apps/web/src/lib/realtime.ts
@@ -1,6 +1,10 @@
 
+export const realtime = {
+  subscribe: (channel: string, callback: (data: any) => void) => ({ unsubscribe: () => {} }),
+  publish: (channel: string, data: any) => {},
+};
 export function subscribeToChannel(channel: string, callback: (data: any) => void) {
   return { unsubscribe: () => {} };
 }
 export function publishToChannel(channel: string, data: any) {}
-export default { subscribeToChannel, publishToChannel };
+export default realtime;
