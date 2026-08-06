9b58c6bc6c90fd83e22631272156fa36aa1b81dc
Scott Eberhardt <eberhardtscott33@gmail.com>
2026-08-06 03:51:41 -0700

    fix: add connect/disconnect to realtime stub

--- apps/web/src/lib/realtime.ts
+++ apps/web/src/lib/realtime.ts
@@ -1,8 +1,13 @@
 export const realtime = {
+  connect: () => {},
+  disconnect: () => {},
   subscribe: (channel: string, callback: (data: any) => void) => ({ unsubscribe: () => {} }),
   publish: (channel: string, data: any) => {},
+  on: (event: string, callback: (data: any) => void) => {},
+  off: (event: string) => {},
 };
 export function subscribeToChannel(channel: string, callback: (data: any) => void) {
   return { unsubscribe: () => {} };
 }
 export function publishToChannel(channel: string, data: any) {}
+export default realtime;
